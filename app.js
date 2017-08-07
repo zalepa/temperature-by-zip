7ba53593fb5bf2bfea45ebc0d0622d0a8c0781a6
George Zalepa <george.zalepa@gmail.com>
2017-08-07 07:43:20 -0400

    Finish basic app

---
+++ app.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const zipdb = require('zippity-do-dah');
+const Forecastio = require('forecastio');
+
+const app = express();
+const weather = new Forecastio('eec7bd3b6816fe8db672ccc3c005be06');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get(/^\/(\d{5})$/, (req, res, next) => {
+  const zipcode = req.params[0];
+  const location = zipdb.zipcode(zipcode);
+  if (!location.zipcode) {
+    next();
+    return;
+  }
+
+  const latitude = location.latitude;
+  const longitude = location.longitude;
+
+  weather.forecast(latitude, longitude, (err, data) => {
+    if (err) {
+      next();
+      return;
+    }
+
+    res.json({
+      zipcode: zipcode,
+      temperature: data.currently.temperature
+    });
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).render('404');
+});
+
+app.listen(3000);
